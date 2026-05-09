a7c971e393cdafa035634785696c608c867ebd6d
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-09 17:41:52 +0545

    Lucide Icon Added

--- frontend/sh-project/src/main.jsx
+++ frontend/sh-project/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Lightbulb } from 'lucide-react'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold text-blue-500">
-        Tailwind v3 Working 🚀
+        Tailwind v3 Working <Lightbulb className="inline-block w-6 h-6 ml-2 text-yellow-500" />
       </h1>
     </div>
   </StrictMode>,
