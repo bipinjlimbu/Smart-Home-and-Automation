1742803fedb0f611212db03b9e49212b524a38e5
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-09 17:24:16 +0545

    Views Added

--- backend/sh_project/sh_app/views.py
+++ backend/sh_project/sh_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import DeviceControl, SensorData
+from .serializers import DeviceControlSerializer, SensorDataSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST', 'PATCH'])
+def device_api(request):
+    state, _ = DeviceControl.objects.get_or_create(id=1)
+
+    if request.method in ['POST', 'PATCH']:
+        serializer = DeviceControlSerializer(state, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "updated", "data": serializer.data})
+        return Response(serializer.errors, status=400)
+
+    serializer = DeviceControlSerializer(state)
+    return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+def sensor_api(request):
+    if request.method == "POST":
+        serializer = SensorDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "saved"})
+        return Response(serializer.errors, status=400)
+
+    latest = SensorData.objects.last()
+    serializer = SensorDataSerializer(latest)
+    return Response(serializer.data)
