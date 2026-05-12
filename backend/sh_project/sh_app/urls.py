5901f9db2a1f8b1472c5780689fe65115e21ee58
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-12 18:53:35 +0545

    Sensor History API Url Added

--- backend/sh_project/sh_app/urls.py
+++ backend/sh_project/sh_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import device_api, sensor_api
+from .views import device_api, sensor_api, sensor_history_api
 
 urlpatterns = [
     path('device/', device_api, name='device_api'),
     path('sensor/', sensor_api, name='sensor_api'),
+    path('sensor/history/', sensor_history_api, name='sensor_history_api'),
 ]
